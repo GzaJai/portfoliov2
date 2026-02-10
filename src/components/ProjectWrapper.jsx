3812170f695d1390a037b21a30e28eaa187836d9
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2026-02-10 00:10:50 -0300

    implementing ProjectDialog

--- src/components/ProjectWrapper.jsx
+++ src/components/ProjectWrapper.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProjectCard from './ProjectCard'
+import ProjectDialog from './ProjectDialog'
 
 const ProjectWrapper = ({ projects }) => {
+  const [open, setOpen] = useState(false)
+  const [selectedProject, setSelectedProject] = useState(null)
+
+  const handleClick = (project) => {
+    setOpen(true);
+    setSelectedProject(project)
+  }
+
   return (
-    <div className='flex flex-col gap-4'>
-        {
+    <>
+      <div className='flex flex-col gap-4'>
+          {
             projects.map((p) => (
-              <ProjectCard key={p.id} projectName={p.title} projectUrl={p.url} imageUrl={p.image}/>
+                <ProjectCard key={p.id} onClick={() => handleClick(p)} projectName={p.title} projectUrl={p.url} imageUrl={p.image}/>
             ))
-        }
-    </div>
+          }
+      </div>
+      <ProjectDialog isOpen={open} onClose={() => setOpen(false)} project={selectedProject}/>
+    </>
   )
 }
 
