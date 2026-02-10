032b32c32a5a217755483090dead8bde831ab6e7
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2026-02-10 12:02:16 -0300

    add arrow icons

---
+++ src/components/icons/CircleArrowUp.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const CircleArrowUp = ({ width, height, stroke, strokeWidth }) => {
+  return (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={width}
+        height={height}
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke={stroke}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        >
+        <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+        <path d="M12 8l-4 4" />
+        <path d="M12 8v8" />
+        <path d="M16 12l-4 -4" />
+    </svg>
+  )
+}
+
+export default CircleArrowUp
