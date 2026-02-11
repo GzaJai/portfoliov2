5731a4604c6409cc128727bffa5817737f846b08
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2026-02-11 16:32:24 -0300

    styling and implementing features

--- src/components/ProjectDialog.jsx
+++ src/components/ProjectDialog.jsx
@@ -1,30 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Accordion from './Accordion'
+import AccordionItem from './AccordionItem'
+import CloseIcon from './icons/CloseIcon'
+import LinkIcon from './icons/LinkIcon'
+import Github from './icons/Github'
+import Tooltip from './Tooltip'
 
-const ProjectDialog = ({ isOpen, onClose, project }) => {
-
-    console.log(project);
-    
+const ProjectDialog = ({ isOpen, onClose, project }) => {  
 
     return (
         <div onClick={onClose} className={`fixed inset-0 flex justify-center items-center transition-colors ${isOpen ? 'visible bg-black/40' : 'invisible'}`}>
-            <div onClick={e => e.stopPropagation()} className={`bg-slate-900 w-[95%] rounded-xl shadow p-6 transition-all ${isOpen ? 'scale-100 opacity-100' : 'scale-125 opacity-0'}`} >
+            <div onClick={e => e.stopPropagation()} className={`bg-slate-900/94 w-[95%] rounded-xl shadow p-6 transition-all ${isOpen ? 'scale-100 opacity-100' : 'scale-125 opacity-0'}`} >
             
             {
                 project &&
-                <div className='flex flex-col gap-4'>
+                <div className='flex flex-col gap-3'>
                     <div className='flex w-[90%] mx-auto justify-between'>
                         <h3 className='text-xl font-semibold'>{project.title}</h3>
                         <a href={project.url}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="#ffffde" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-                            >
-                                <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6" />
-                                <path d="M11 13l9 -9" />
-                                <path d="M15 4h5v5" />
-                            </svg>
                         </a>
+                        <span onClick={onClose}>
+                            <CloseIcon width={'38'} height={'38'}/>
+                        </span>
                     </div>
                     <img src={`https://raw.githubusercontent.com/GzaJai/portfolio/main/public/images/${project.image}.png`} alt="" />
-                    <p>{project.description}</p>
+                    <div className='flex w-1/2 mx-auto justify-around pb-4'>
+                        <Tooltip textContent={'Repository'}>
+                            <Github width={32} height={32} stroke={'#ffffff'} strokeWidth={'2'} />
+                        </Tooltip>
+                        <Tooltip textContent={'Website'}>
+                            <LinkIcon width={32} height={32} stroke={'#ffffff'} strokeWidth={'2'} />
+                        </Tooltip>
+                    </div>
+                    <Accordion>
+                        <AccordionItem title={'Stack'}>
+                            * React
+                            * Tailwindcss
+                            * JavaScript
+                            </AccordionItem>
+                            <AccordionItem title={'Purpose'}>
+                            Entretainment, practice
+                            </AccordionItem>
+                            <AccordionItem title={'Development'}>
+                            Started with some research of other dev's portfolios for some inspo. 
+                            Then I design the structure of it...
+                        </AccordionItem>
+                    </Accordion>
                 </div>
             }
 
