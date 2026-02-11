87c1b20de7049e6aa0c980fe0aab2a2ed7536db6
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2026-02-11 15:15:55 -0300

    implementing animated links

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import ProjectCard from './components/ProjectCard';
 import dataFromJson from './data.json'
 import ProjectWrapper from './components/ProjectWrapper';
 import { AnimatedBackground } from './components/AnimatedBackground';
+import { motion } from 'framer-motion';
+import AnimatedLink from './components/AnimatedLink';
+import Linkedin from './components/icons/Linkedin';
+import Github from './components/icons/Github';
 
 function App() {
   const [scrolled, setScrolled] = useState(false);
@@ -50,22 +54,12 @@ function App() {
             <h2 className='text-2xl font-bold'>Gonzalo Jaime</h2>
             <h3 className='text-2xl font-semibold mt-2'>Developer</h3>
             <div className='flex w-[50%] justify-center gap-4 mt-8'>
-              <a target='_blank' href="https://www.linkedin.com/in/gjaimeguinazu/">
-                <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="#7c86ff" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"
-                >
-                  <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
-                  <path d="M8 11l0 5" />
-                  <path d="M8 8l0 .01" />
-                  <path d="M12 16l0 -5" />
-                  <path d="M16 16v-3a2 2 0 0 0 -4 0" />
-                </svg>
-              </a>
-              <a target='_blank' href="https://github.com/GzaJai">
-                <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="#7c86ff" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"
-                  >
-                  <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
-                </svg>
-              </a>
+              <AnimatedLink href={"https://www.linkedin.com/in/gjaimeguinazu/"}>
+                <Linkedin width={'64'} height={'64'} stroke={'#7c86ff'} strokeWidth={'1.5'}/>
+              </AnimatedLink>
+              <AnimatedLink href={"https://github.com/GzaJai"}>
+                <Github width={'64'} height={'64'} stroke={'#7c86ff'} strokeWidth={'1.5'}/>
+              </AnimatedLink>
             </div>
           </div>
           
