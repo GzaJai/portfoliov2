8bcf0130459b3bfbbb44eca2d4bfe0e0b102b7bd
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2026-02-11 16:31:44 -0300

    create link icon

---
+++ src/components/icons/LinkIcon.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const LinkIcon = ({ width, height, stroke, strokeWidth }) => {
+  return (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={width}
+        height={height}
+        stroke={stroke}
+        strokeWidth={strokeWidth}
+        viewBox="0 0 24 24"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+    >
+      <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6" />
+      <path d="M11 13l9 -9" />
+      <path d="M15 4h5v5" />
+    </svg>
+  );
+};
+
+export default LinkIcon;
