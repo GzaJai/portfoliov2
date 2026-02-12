3c438b097ecd0528d989591f573684f2f9510b8c
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2026-02-12 12:40:36 -0300

    implementing translation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 import './App.css'
 import CustomLink from './components/CustomLink'
 import dataFromJson from './data.json'
@@ -15,6 +16,8 @@ function App() {
   const [scrolled, setScrolled] = useState(false);
   const [projects, setProjects] = useState([])
   const [showScrollTop, setShowScrollTop] = useState(false)
+
+  const { t } = useTranslation();
 
   useEffect(() => {
     if (dataFromJson) {
@@ -47,16 +50,16 @@ function App() {
     return (
       <>
       <section id='home' className='w-full'>
-        <header className={`duration-200 rounded-b-2xl w-[80%] ${scrolled ? "backdrop-blur-md" : "mt-4"}`}>
+        <header className={`duration-200 rounded-b-2xl w-[90%] ${scrolled ? "backdrop-blur-md" : "mt-4"}`}>
             <div className='flex h-20 w-full px-20 items-center justify-between'>
               <div className='max-md:hidden'>
                 <img className='w-20  bg-indigo-400 p-[.1rem] rounded-full max-md:hidden' src='https://raw.githubusercontent.com/GzaJai/portfoliov2/refs/heads/master/public/yo-portfolio.png'/>
                 <p className='ml-10 w-[18rem] text-4xl font-bold'>Gonzalo Jaime</p>
               </div>
               <nav className="flex w-full items-center justify-center z-50">
-                <li><CustomLink text={"About"} href={"#about"} /></li>
-                <li><CustomLink text={"Projects"} href={"#projects"} /></li>
-                <li><CustomLink text={"Contact"} href={"#contact"} /></li>
+                <li><CustomLink text={t("nav.about")} href={"#about"} /></li>
+                <li><CustomLink text={t("nav.projects")} href={"#projects"} /></li>
+                <li><CustomLink text={t("nav.contact")} href={"#contact"} /></li>
               </nav>
             </div>
         </header>
@@ -66,7 +69,7 @@ function App() {
           <div className="flex flex-col my-54 items-center gap-4">
             <img className='w-60 bg-indigo-500/40 p-4 rounded-full backdrop-blur-glass backdrop-filter backdrop-blur-md' src='https://raw.githubusercontent.com/GzaJai/portfoliov2/refs/heads/master/public/yo-portfolio.png' />
             <h2 className='text-2xl font-bold'>Gonzalo Jaime</h2>
-            <h3 className='text-2xl font-semibold mt-2'>Developer</h3>
+            <h3 className='text-2xl font-semibold mt-2'>{t("developer")}</h3>
             <div className='flex w-[50%] justify-center gap-4 mt-8'>
               <AnimatedLink href={"https://www.linkedin.com/in/gjaimeguinazu/"}>
                 <Linkedin width={'64'} height={'64'} stroke={'#7c86ff'} strokeWidth={'1.5'}/>
@@ -79,13 +82,13 @@ function App() {
           
         </section>
         <section id='about' className='mb-48'>
-          <h3 className='w-[60%] mx-auto mb-7 font-bold text-3xl text-indigo-300 md:w-[50%]'>About me</h3>
+          <h3 className='w-[60%] mx-auto mb-7 font-bold text-3xl text-indigo-300 md:w-[50%]'>{t("aboutMe.title")}</h3>
           <div className="w-[90%] mx-auto bg-linear-to-br from-indigo-500/20 via-purple-500/10 to-transparent backdrop-blur-md rounded-[39px] shadow-2xl p-12">
             <div className='flex flex-col w-full gap-3'>
-                <p>Hello, I'm Gonzalo Jaime. A full-stack developer from Argentina. </p>
-                <p>Currently I'm studying at the University, but I have done some development courses that give me a pretty solid stack.</p>
-                <p>I got some experience as a freelancer as well. I've done some projects for myself to improve and reinforce my skills, and some other to sell.</p>
-                <p className='text-center font-semibold pt-8 pb-4'>checkout my CV!</p>
+                <p>{t("aboutMe.text1")}</p>
+                <p>{t("aboutMe.text2")}</p>
+                <p>{t("aboutMe.text3")}</p>
+                <p className='text-center font-semibold pt-8 pb-4'>{t("aboutMe.cvText")}</p>
               </div>
               <AnimatedLink className={'mx-auto w-full'} href={"https://drive.google.com/file/d/1redMsxAEaUpyPs-slkqpAG9fZxGjFUbX/view?pli=1"}>
                 <CV width={'64'} height={'64'} stroke={'#ffffffde'} strokeWidth={'1.5'} />
@@ -94,15 +97,15 @@ function App() {
 
         </section>
         <section id="projects" className='mt-10'>
-          <h3 className='w-[60%] mx-auto mb-7 font-bold text-3xl text-indigo-300 md:w-[50%]'>Projects</h3>
-          <ProjectWrapper projects={projects}/>
+          <h3 className='w-[60%] mx-auto mb-7 font-bold text-3xl text-indigo-300 md:w-[50%]'>{t("projects.title")}</h3>
+          <ProjectWrapper seeAllText={t("projects.seeAllBtn")} seeLessText={t("projects.seeLessBtn")} projects={projects}/>
         </section>
-        <section id='#contact' className='my-18'>
-          <ContactForm />
+        <section id='contact' className='my-18'>
+          <ContactForm translationText={t("contact")} />
         </section>
-        <section className='w-full h-12 text-center'>
+        <section id='footer' className='w-full h-12 text-center'>
             <a href="https://www.linkedin.com/in/gjaimeguinazu/" target='_blank'>Gonzalo Jaime </a>
-             - made with ❤️
+             - {t("footer")} ❤️
         </section>
       </main>
       <ScrollToTop isVisible={showScrollTop} />
