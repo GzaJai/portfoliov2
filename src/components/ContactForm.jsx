749c31c7ec1d8f08af7f51c16ced41e7b596e7e5
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2026-02-12 12:36:32 -0300

    implementing translation

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import emailjs from '@emailjs/browser'
 import { motion, AnimatePresence } from 'framer-motion'
 import AnimatedLoadingIcon from './icons/AnimatedLoadingIcon'
@@ -6,6 +7,9 @@ import AnimatedLoadingIcon from './icons/AnimatedLoadingIcon'
 const ContactForm = () => {
     const [loading, setLoading] = useState(false)
     const [notification, setNotification] = useState(null)
+
+    const { t } = useTranslation();
+    
 
     const showNotification = (type, message) => {
         setNotification({ type, message })
@@ -65,19 +69,19 @@ const ContactForm = () => {
                 )}
             </AnimatePresence>
 
-            <h3 className='w-[60%] mx-auto mb-7 font-bold text-3xl text-indigo-300 md:w-[50%]'>Contact me</h3>
+            <h3 className='w-[60%] mx-auto mb-7 font-bold text-3xl text-indigo-300 md:w-[50%]'>{t("contact.title")}</h3>
             <form onSubmit={handleSubmit} className="flex flex-col w-[90%] mx-auto bg-linear-to-br from-indigo-500/20 via-purple-500/10 to-transparent backdrop-blur-md rounded-[39px] shadow-2xl py-12 px-6 items-center">
-                <h4 className='font-bold text-xl pb-8'>Send me a message</h4>
+                <h4 className='font-bold text-xl pb-8'>{t("contact.title2")}</h4>
                 <div className="p-4 flex flex-col w-[90%] gap-3">
-                    <label htmlFor="from_name" className='font-bold'>Name</label>
-                    <input type="text" name="from_name" id="from_name" className='h-8 rounded-md max-w-70 p-3 border border-indigo-100/40' placeholder='John Doe'  />
+                    <label htmlFor="from_name" className='font-bold'>{t("contact.nameLabel")}</label>
+                    <input type="text" name="from_name" id="from_name" className='h-8 rounded-md max-w-70 p-3 border border-indigo-100/40' placeholder={t("contact.namePlaceholder")}  />
                 </div>
                 <div className="p-4 flex flex-col w-[90%] gap-3">
-                    <label htmlFor="reply_to" className='font-bold'>Email</label>
-                    <input type="text" name="reply_to" id="reply_to" className='h-8 rounded-md max-w-70 p-3 border border-indigo-100/40' placeholder='johndoe@example.com'  />
+                    <label htmlFor="reply_to" className='font-bold'>{t("contact.emailLabel")}</label>
+                    <input type="text" name="reply_to" id="reply_to" className='h-8 rounded-md max-w-70 p-3 border border-indigo-100/40' placeholder={t("contact.emailPlaceholder")}  />
                 </div>
                 <div className="p-4 flex flex-col w-[90%] gap-3">
-                    <label htmlFor="message" className='font-bold'>Message</label>
+                    <label htmlFor="message" className='font-bold'>{t("contact.messageLabel")}</label>
                     <textarea name="message" id="message" className='h-24 rounded-md p-3 border border-indigo-100/40' />
                 </div>
 
@@ -86,7 +90,7 @@ const ContactForm = () => {
                     disabled={loading}
                     className='flex items-center justify-center w-1/2 h-16 font-bold text-center bg-indigo-600 duration-500 ease-in-out  hover:bg-indigo-200/70 hover:text-indigo-600 hover:cursor-pointer rounded-lg p-4 mt-6 disabled:opacity-50 disabled:cursor-not-allowed'
                 >
-                    {loading ? <AnimatedLoadingIcon /> : 'Send Message'}
+                    {loading ? <AnimatedLoadingIcon /> : t("contact.sendBtn")}
                 </button>
             </form>
         </>
