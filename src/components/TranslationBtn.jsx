1a078a018e64eac750f5b6eab95c0a89dc220d63
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2026-02-12 13:11:14 -0300

    add translation button

---
+++ src/components/TranslationBtn.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
+import ArgentinaFlag from './icons/ArgentinaFlag'
+import UkFlag from './icons/UkFlag'
+
+const TranslationBtn = () => {
+
+    const [currentLng, setCurrentLng] = useState("")
+    const { i18n } = useTranslation();
+
+    useEffect(() => {
+        setCurrentLng(i18n.language)
+    }, []);
+
+    const handleLngSwitch = () => {
+        if (currentLng=="es"){
+            setCurrentLng("en")
+            i18n.changeLanguage("en")
+        } else if (currentLng=="en") {
+            setCurrentLng("es")
+            i18n.changeLanguage("es")
+        }
+    }
+
+  return (
+    <button onClick={handleLngSwitch} className="fixed bottom-8 right-8 z-9999 p-3 bg-indigo-600 hover:bg-indigo-500 text-white rounded-full shadow-2xl backdrop-blur-sm border border-indigo-400/30">
+        { currentLng=="es"&&<ArgentinaFlag width={40} height={40} />}
+        { currentLng=="en"&&<UkFlag width={40} height={40} />}
+    </button>
+  )
+}
+
+export default TranslationBtn
