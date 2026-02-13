4a70def899d777cc0a09af48b6ae8d88be4d191c
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2026-02-13 01:24:44 -0300

    restyling for desktop view

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -69,26 +69,26 @@ const ContactForm = () => {
                 )}
             </AnimatePresence>
 
-            <h3 className='w-[60%] mx-auto mb-7 font-bold text-3xl text-indigo-300 md:w-[50%]'>{t("contact.title")}</h3>
-            <form onSubmit={handleSubmit} className="flex flex-col w-[90%] mx-auto bg-linear-to-br from-indigo-500/20 via-purple-500/10 to-transparent backdrop-blur-md rounded-[39px] shadow-2xl py-12 px-6 items-center">
+            <h3 className='w-[60%] mx-auto mb-7 font-bold text-3xl text-indigo-300 md:w-[40%]'>{t("contact.title")}</h3>
+            <form onSubmit={handleSubmit} className="flex flex-col w-[90%] mx-auto bg-linear-to-br from-indigo-500/20 via-purple-500/10 to-transparent backdrop-blur-md rounded-[39px] shadow-2xl py-12 px-6 items-center md:w-[40%]">
                 <h4 className='font-bold text-xl pb-8'>{t("contact.title2")}</h4>
-                <div className="p-4 flex flex-col w-[90%] gap-3">
-                    <label htmlFor="from_name" className='font-bold'>{t("contact.nameLabel")}</label>
-                    <input type="text" name="from_name" id="from_name" className='h-8 rounded-md max-w-70 p-3 border border-indigo-100/40' placeholder={t("contact.namePlaceholder")}  />
+                <div className="p-4 flex flex-col w-[90%] gap-3 md:w-[75%]">
+                    <label htmlFor="from_name" className='font-bold w-[40%]'>{t("contact.nameLabel")}</label>
+                    <input type="text" name="from_name" id="from_name" className='h-8 rounded-md max-w-70 p-3 border border-indigo-100/40 md:w-full' placeholder={t("contact.namePlaceholder")}  />
                 </div>
-                <div className="p-4 flex flex-col w-[90%] gap-3">
-                    <label htmlFor="reply_to" className='font-bold'>{t("contact.emailLabel")}</label>
-                    <input type="text" name="reply_to" id="reply_to" className='h-8 rounded-md max-w-70 p-3 border border-indigo-100/40' placeholder={t("contact.emailPlaceholder")}  />
+                <div className="p-4 flex flex-col w-[90%] gap-3 md:w-[75%]">
+                    <label htmlFor="reply_to" className='font-bold w-[40%]'>{t("contact.emailLabel")}</label>
+                    <input type="text" name="reply_to" id="reply_to" className='h-8 rounded-md max-w-70 p-3 border border-indigo-100/40 md:w-full' placeholder={t("contact.emailPlaceholder")}  />
                 </div>
-                <div className="p-4 flex flex-col w-[90%] gap-3">
-                    <label htmlFor="message" className='font-bold'>{t("contact.messageLabel")}</label>
-                    <textarea name="message" id="message" className='h-24 rounded-md p-3 border border-indigo-100/40' />
+                <div className="p-4 flex flex-col w-[90%] gap-3 md:w-[75%]">
+                    <label htmlFor="message" className='font-bold w-[40%]'>{t("contact.messageLabel")}</label>
+                    <textarea name="message" id="message" className='h-24 rounded-md p-3 border border-indigo-100/40 md:h-48 md:w-128' />
                 </div>
 
                 <button 
                     type='submit' 
                     disabled={loading}
-                    className='flex items-center justify-center w-1/2 h-16 font-bold text-center bg-indigo-600 duration-500 ease-in-out  hover:bg-indigo-200/70 hover:text-indigo-600 hover:cursor-pointer rounded-lg p-4 mt-6 disabled:opacity-50 disabled:cursor-not-allowed'
+                    className='flex items-center justify-center w-1/2 h-16 font-bold text-center bg-indigo-600 duration-500 ease-in-out  hover:bg-indigo-200/70 hover:text-indigo-600 hover:cursor-pointer rounded-lg p-4 mt-6 disabled:opacity-50 disabled:cursor-not-allowed md:w-70'
                 >
                     {loading ? <AnimatedLoadingIcon /> : t("contact.sendBtn")}
                 </button>
